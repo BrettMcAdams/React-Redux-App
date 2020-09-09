62a322bb371e52701e9306758ae3ea7725b5ed89
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-09-09 16:45:55 -0400

    started working on state management with redux bp

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Breaking Bad Characters</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+import './index.css';
+import App from './App';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
